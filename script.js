e90f86b6ae42b466378a9c5b8b45083545133685
steafan27 <steafansteinocher@gmail.com>
2025-09-15 09:40:22 +0200

    If applied this commit will generate the computers choice.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+function generateRandomNumber(){
+    let randomNumber = Math.floor((Math.random() * 3) + 1); //numbers 1 - 3
+    return randomNumber;
+}
+
+function getComputerChoice(){
+    let num = generateRandomNumber();
+    switch(num){
+        case 1: return "Rock";
+        case 2: return "Paper";
+        case 3: return "Scissors";
+    }
+
+}
+
+console.log(getComputerChoice());
